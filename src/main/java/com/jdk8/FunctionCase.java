c1d7104b214bb500275c2104d588029e5d5dac36
ZeroWM <wm4968@gmail.com>
2019-12-23 14:01:17 +0800

    feat(dependency): add

--- src/main/java/com/jdk8/FunctionCase.java
+++ src/main/java/com/jdk8/FunctionCase.java
@@ -1,9 +1,5 @@
 package com.jdk8;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.function.Function;
-
 public class FunctionCase {
     /**
      * 接口类型：consumer supplier predicate  function
@@ -15,35 +11,6 @@ public class FunctionCase {
      * function 接口传入一个参数，返回一个值
      */
 
-    private HtmlImageURLProxier proxy;
 
-    @Autowired
-    public FunctionCase(HtmlImageURLProxier proxy) {
-        this.proxy = proxy;
-    }
-
-    public void run() {
-        proxy.process("http://www. baidu. com", src -> {
-            return println(src);
-        });
-    }
-
-    private String println(String word) {
-        return word.trim();
-    }
 }
 
-
-class HtmlImageURLProxierImpl implements HtmlImageURLProxier {
-
-    @Override
-    public String process(String content, Function<String, String> relativeUrlTransformer) {
-        String finalStr = content + "?username = wm";
-        relativeUrlTransformer.apply(content);
-        return finalStr;
-    }
-}
-
-interface HtmlImageURLProxier {
-    String process(String content, Function<String, String> relativeUrlTransformer);
-}
