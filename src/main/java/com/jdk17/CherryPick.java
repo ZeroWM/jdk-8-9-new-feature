0fdcb94cf2bf6309acd6d74fddba82f84c61028b
ZeroWM <wm4968@gmail.com>
2023-05-08 19:12:23 +0800

    feat: rebase test

--- src/main/java/com/jdk17/CherryPick.java
+++ src/main/java/com/jdk17/CherryPick.java
@@ -2,8 +2,9 @@ package main.java.com.jdk17;
 
 public class CherryPick {
   public static void main(String[] args) {
-    System.out.println("a");
-    System.out.println("b");
     System.out.println("d");
+  }
+
+  public void test(){
   }
 }
