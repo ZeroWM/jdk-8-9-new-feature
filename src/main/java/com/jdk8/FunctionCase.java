d36da86863f32873d0fce5a129f270a0cae4a4dd
ZeroWM <wm4968@gmail.com>
2025-07-15 18:59:11 +0800

    Revert "add annotations"
    This reverts commit 6427490a93643fbe92e1bbed3bb35c9c3d39866e.

--- src/main/java/com/jdk8/FunctionCase.java
+++ src/main/java/com/jdk8/FunctionCase.java
@@ -1,12 +1,16 @@
 package com.jdk8;
 
 public class FunctionCase {
-  /**
-   * 接口类型：consumer supplier predicate function
-   *
-   * <p>区别： consumer 泛型参数 不返回值 supplier 不传参 返回一个值 predicate 接口传一个参数 返回一个布尔值 function 接口传入一个参数，返回一个值
-   */
+    /**
+     * 接口类型：consumer supplier predicate  function
+     * <p>
+     * 区别：
+     * consumer 泛型参数 不返回值
+     * supplier 不传参 返回一个值
+     * predicate 接口传一个参数 返回一个布尔值
+     * function 接口传入一个参数，返回一个值
+     */
 
-  // abc
 
 }
+
